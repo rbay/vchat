d3b334fca33fc9f1d7ad75df20d8de7d7ab82b30
sbupathi <sbupathi@ubuntu.ubuntu-domain>
2013-01-24 01:23:49 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vchat::Application.config.secret_token = 'fddcf41de8dffaf952b19dd993c8a717403630adf22b2ca9af01c84a86989c1f7acab4e684977f3e3514dbf1c039636aad1eeb28ba98f40e58c7d8fd6fa53ce6'

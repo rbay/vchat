0d75ead3f5bebc209d6f64fd2fe7cb2335ae3416
sbupathi <sbupathi@ubuntu.ubuntu-domain>
2013-01-27 00:35:30 +0530

    Minor

--- app/controllers/vchat_controller.rb
+++ app/controllers/vchat_controller.rb
@@ -3,9 +3,22 @@ class VchatController < ApplicationController
         @api_key = "22676642"        # Replace with your OpenTok API key.
         api_secret = "5f88a4d72917d94131c7bdb0d8bb0e8b0108c12d"  # Replace with your OpenTok API secret.
 
-        p opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
-        p session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "enabled"}
-        p @session = opentok.create_session(request.remote_addr, session_properties)
-        p @token = opentok.generate_token(:session_id => @session)
+        if stream = Stream.first
+        	p '#######'
+        	p stream
+	        p @session = stream.stream_session_id
+	        	    	opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
+
+	        p @token = opentok.generate_token(:session_id => @session)
+
+	    else
+	    	p '************'
+	    	opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
+	        session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "enabled"}
+	        p @session = opentok.create_session(request.remote_addr, session_properties)
+	        @token = opentok.generate_token(:session_id => @session)
+	        p s = Stream.new(:stream_session_id => @session.session_id, :token =>@token)
+	        p s.save
+	   	end
     end
 end

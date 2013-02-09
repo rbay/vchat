320c093d43149254341d9e28757eeb3aa909ac16
sbupathi <sbupathi@ubuntu.ubuntu-domain>
2013-02-09 21:19:04 +0530

    Added tx chat

--- app/controllers/vchat_controller.rb
+++ app/controllers/vchat_controller.rb
@@ -1,29 +1,31 @@
 class VchatController < ApplicationController
 	def index
-        @api_key = "22676642"        # Replace with your OpenTok API key.
-        api_secret = "5f88a4d72917d94131c7bdb0d8bb0e8b0108c12d"  # Replace with your OpenTok API secret.
+		if params[:name]
+	        @api_key = "22676642"        # Replace with your OpenTok API key.
+	        api_secret = "5f88a4d72917d94131c7bdb0d8bb0e8b0108c12d"  # Replace with your OpenTok API secret.
 
-        # if Stream.first
-        	# p '#######'
-        	# p stream
-	        # p @session = stream.stream_session_id
-	        # 	    	opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
+	        # if Stream.first
+	        	# p '#######'
+	        	# p stream
+		        # p @session = stream.stream_session_id
+		        # 	    	opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
 
-	        # p @token = opentok.generate_token(:session_id => @session)
+		        # p @token = opentok.generate_token(:session_id => @session)
 
-	    # else
-
-	        if params[:stream]
-	        	@session = Stream.find(params[:stream]).stream_session_id.to_s
-	        	@session_token = Stream.find(params[:stream]).session_token
-	        else
-	        	opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
-	        	session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "enabled"}
-	        	@session = opentok.create_session(request.remote_addr, session_properties)
-	        	test= @session.session_id
-	        	@session_token = opentok.generate_token(:session_id => @session)
-	        end
-	        @s = Stream.create(:stream_session_id => test, :session_token =>@session_token.to_s)
-	   	# end
+		    # else
+		        if params[:stream]
+		        	@session = Stream.find(params[:stream]).stream_session_id.to_s
+		        	@session_token = Stream.find(params[:stream]).session_token
+		        else
+		        	opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
+		        	session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "enabled"}
+		        	@session = opentok.create_session(request.remote_addr, session_properties)
+		        	session_id = @session.session_id
+		        	@session_token = opentok.generate_token(:session_id => @session)
+		        	@publisher = true
+		        end
+		        @s = Stream.create(:stream_session_id => session_id, :session_token =>@session_token.to_s)
+	   		# end
+	   	end
     end
 end

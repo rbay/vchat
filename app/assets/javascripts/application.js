181545178863d905bda048dbcc352cae70e8ee69
sbupathi <sbupathi@ubuntu.ubuntu-domain>
2013-02-22 01:41:05 +0530

    code refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,260 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$(document).ready(function(){
+	var app_data = $("#app-data"), app = {
+		name: app_data.data('name'),
+		age: app_data.data('age'),
+		gender: app_data.data('gender'),
+		bio: app_data.data('bio')
+	};
+
+	if (app.name !== null){
+		console.log("test")
+		var apiKey = app_data.data('api'), 
+		sessionId = app_data.data('session'),
+		token = app_data.data('token');
+
+		var session, publisher;
+		var subscribers = {};
+		var VIDEO_WIDTH = 320;
+		var VIDEO_HEIGHT = 240;
+
+		TB.addEventListener("exception", exceptionHandler);
+		
+		// Un-comment the following to set automatic logging:
+		// TB.setLogLevel(TB.DEBUG);
+
+		if (TB.checkSystemRequirements() != TB.HAS_REQUIREMENTS) {
+			alert("You don't have the minimum requirements to run this application."
+				  + "Please upgrade to the latest version of Flash.");
+		} else {
+			session = TB.initSession(sessionId);	// Initialize session
+
+			// Add event listeners to the session
+			session.addEventListener('sessionConnected', sessionConnectedHandler);
+			session.addEventListener('sessionDisconnected', sessionDisconnectedHandler);
+			session.addEventListener('connectionCreated', connectionCreatedHandler);
+			session.addEventListener('connectionDestroyed', connectionDestroyedHandler);
+			session.addEventListener('streamCreated', streamCreatedHandler);
+			session.addEventListener('streamDestroyed', streamDestroyedHandler);
+		}
+
+		//--------------------------------------
+		//  LINK CLICK HANDLERS
+		//--------------------------------------
+
+		/*
+		If testing the app from the desktop, be sure to check the Flash Player Global Security setting
+		to allow the page from communicating with SWF content loaded from the web. For more information,
+		see http://www.tokbox.com/opentok/build/tutorials/helloworld.html#localTest
+		*/
+		function connect() {
+			session.connect(apiKey, token);	
+			console.log(session.subscribers);
+
+		}
+
+		function disconnect() {
+			session.disconnect();
+			hide('disconnectLink');
+			hide('publishLink');
+			hide('unpublishLink');
+		}
+
+		$("#disconnectLink").on('click', function() {
+			disconnect();
+		});
+
+		$("#connectLink").on('click', function() {
+			connect();
+		});
+
+		// Called when user wants to start publishing to the session
+		function startPublishing() {
+			console.log(session);
+			if (!publisher) {
+				$("#myCamera").html("");
+				var parentDiv = document.getElementById("myCamera");
+				var publisherDiv = document.createElement('div'); // Create a div for the publisher to replace
+				publisherDiv.setAttribute('id', 'opentok_publisher');
+				parentDiv.appendChild(publisherDiv);
+				var publisherProps = {width: VIDEO_WIDTH, height: VIDEO_HEIGHT};
+				publisher = TB.initPublisher(apiKey, publisherDiv.id, publisherProps);  // Pass the replacement div id and properties
+				session.publish(publisher, {name: "John Doe"} );
+				show('unpublishLink');
+				hide('publishLink');
+			}
+		}
+
+		function stopPublishing() {
+			if (publisher) {
+				session.unpublish(publisher);
+			}
+			publisher = null;
+
+			show('publishLink');
+			hide('unpublishLink');
+		}
+
+		//--------------------------------------
+		//  OPENTOK EVENT HANDLERS
+		//--------------------------------------
+
+		function sessionConnectedHandler(event) {
+			for (var i = 0; i < event.streams.length; i++) {
+				addStream(event.streams[i]);
+			}
+			show('disconnectLink');
+			show('publishLink');
+			hide('connectLink');
+			show('session_id');
+			show('joinStream');
+			startPublishing();
+			var stateManager = session.getStateManager();
+			stateManager.addEventListener("changed", stateChangedHandler);
+		}
+
+		function stateChangedHandler(event) {
+			console.log(event);
+		    for (key in event.changedValues) {
+		        // alert("Changed state. Key: " + key + ". Value: " + event.changedValues[key]);
+		        console.log($(".chat nav-list"));
+		        $(".chat .nav-list").append("<li style='padding: 10px 0'><span style='color: #2fa4e7'>" + key + ":</span>" + event.changedValues[key] +"</li>").promise().done(function() {
+				    $('.chat .nav').animate({
+         				scrollTop: $(".chat .nav li:last").offset().top
+     				}, 1000);
+				});
+		    }
+		}		
+
+		function streamCreatedHandler(event) {
+			// Subscribe to the newly created streams
+			for (var i = 0; i < event.streams.length; i++) {
+				addStream(event.streams[i]);
+			}
+		}
+
+		function streamDestroyedHandler(event) {
+			// This signals that a stream was destroyed. Any Subscribers will automatically be removed.
+			// This default behaviour can be prevented using event.preventDefault()
+		}
+
+		function sessionDisconnectedHandler(event) {
+			// This signals that the user was disconnected from the Session. Any subscribers and publishers
+			// will automatically be removed. This default behaviour can be prevented using event.preventDefault()
+			publisher = null;
+
+			show('connectLink');
+			hide('disconnectLink');
+			hide('publishLink');
+			hide('unpublishLink');
+		}
+
+		function connectionDestroyedHandler(event) {
+			// This signals that connections were destroyed
+		}
+
+		function connectionCreatedHandler(event) {
+			// This signals new connections have been created.
+		}
+
+		/*
+		If you un-comment the call to TB.setLogLevel(), above, OpenTok automatically displays exception event messages.
+		*/
+		function exceptionHandler(event) {
+			alert("Exception: " + event.code + "::" + event.message);
+		}
+
+		//--------------------------------------
+		//  HELPER METHODS
+		//--------------------------------------
+
+		function addStream(stream) {
+			// Check if this is the stream that I am publishing, and if so do not publish.
+			if (stream.connection.connectionId == session.connection.connectionId) {
+				return;
+			}
+			var subscriberDiv = document.createElement('div'); // Create a div for the subscriber to replace
+			subscriberDiv.setAttribute('id', stream.streamId); // Give the replacement div the id of the stream as its id.
+			document.getElementById("subscribers").appendChild(subscriberDiv);
+			var subscriberProps = {width: VIDEO_WIDTH, height: VIDEO_HEIGHT};
+			subscribers[stream.streamId] = session.subscribe(stream, subscriberDiv.id, subscriberProps);
+		}
+
+		$("#joinStream").on("click", function(){
+			$("#connect_container").show();
+		});
+
+		function show(id) {
+			document.getElementById(id).style.display = 'block';
+		}
+
+		function hide(id) {
+			document.getElementById(id).style.display = 'none';
+		}
+
+		// function nextStream(){
+		// 	var streams = <%= params[:streams] %>;
+		// 	for (var i = 0; i < streams.length; i++) {
+		// 		session.connect(apiKey, streams[i][2]);	
+		// 	}
+		// }	
+
+		show('connectLink');	
+	}
+
+	$(".join_chat").click(function(){
+		if ($(".chat_id").val().length === 0){
+			alert("Enter chat id");
+		}else{
+			window.location.href = "/vchat?stream=" + $(".chat_id").val() + "&name=" + app.name + "&age=" + app.age + "&gender=" + app.gender + "&bio=" + app.bio;				
+		}
+		return false;
+	});
+
+	$(".chat-form").submit(function(e){
+		e.preventDefault();
+		var txt = $(".chat-input").val();
+		var stateManager = session.getStateManager();
+		stateManager.set(name, txt);
+		$(".chat-input").val("");
+		return false;
+	});
+
+
+	app.name ? connect() : $('#infoModal').modal('show');
+
+	// Popover 
+	$('#infoForm input').focus(function(){
+		$(this).popover('show');	
+	}).blur(function(){
+		$(this).popover('hide');	
+	});
+
+	// Validation
+	$("#infoForm").validate({
+		rules:{
+			name:{required:true},
+			age:{required:true},
+			bio:{required:true}
+		},
+		messages:{
+			user_name:{ required:"Enter your name" },
+			age:{ required:"Enter your age" },
+			bio:{ required:"Enter your bio"}
+		},
+
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass){
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass){
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});		
+});
--- app/controllers/vchat_controller.rb
+++ app/controllers/vchat_controller.rb
@@ -3,7 +3,8 @@ class VchatController < ApplicationController
 		if params[:name]
 	        @api_key = "22676642"        # Replace with your OpenTok API key.
 	        api_secret = "5f88a4d72917d94131c7bdb0d8bb0e8b0108c12d"  # Replace with your OpenTok API secret.
-
+	        p 'jjjjjjjjjjjjjjjj'
+	        p params[:streams] = Stream.find(:all).map{|stream| [stream.id, stream.stream_session_id.to_s, stream.session_token]}.to_json.html_safe
 	        # if Stream.first
 	        	# p '#######'
 	        	# p stream
@@ -17,14 +18,21 @@ class VchatController < ApplicationController
 		        	@session = Stream.find(params[:stream]).stream_session_id.to_s
 		        	@session_token = Stream.find(params[:stream]).session_token
 		        else
-		        	opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
-		        	session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "enabled"}
-		        	@session = opentok.create_session(request.remote_addr, session_properties)
-		        	session_id = @session.session_id
-		        	@session_token = opentok.generate_token(:session_id => @session)
-		        	@publisher = true
+		        	if session[:tok_session]
+			        	@session = session[:tok_session]
+			        	@session_token = session[:tok_session_token]
+			        	@publisher = true
+			        	@s = session[:stream]
+			        else
+			        	opentok = OpenTok::OpenTokSDK.new(@api_key, api_secret)
+			        	session_properties = {OpenTok::SessionPropertyConstants::P2P_PREFERENCE => "enabled"}
+			        	@session = session[:tok_session] = opentok.create_session(request.remote_addr, session_properties)
+			        	session_id = @session.session_id
+			        	@session_token = session[:tok_session_token] = opentok.generate_token(:session_id => @session)
+			        	@publisher = true
+			        	@s = session[:stream] = Stream.create(:stream_session_id => session_id, :session_token =>@session_token.to_s)
+			        end
 		        end
-		        @s = Stream.create(:stream_session_id => session_id, :session_token =>@session_token.to_s)
 	   		# end
 	   	end
     end
